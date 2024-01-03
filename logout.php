d98e04d9a47f5adb2e7e94a2b3fc51a36410b4bf
Choong Qian Yu <92418780+ChoongQianYu-286623@users.noreply.github.com>
2024-01-04 00:57:01 +0800

    Logout.php

---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php
+include('dbconnect.php');
+session_start(); // Start the session
+
+// Unset all of the session variables
+$_SESSION = array();
+
+// Destroy the session
+session_destroy();
+
+// Redirect to the login page or any other page after logout
+header("Location: index.html");
+exit();
+?>
