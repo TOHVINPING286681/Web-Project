b28236e4d53124a29b4c256e09765a47d272b01f
Choong Qian Yu <92418780+ChoongQianYu-286623@users.noreply.github.com>
2024-01-05 17:54:17 +0800

    Update edit_item.php

--- edit_item.php
+++ edit_item.php
@@ -105,17 +105,19 @@ if (isset($_GET['item_id'])) {
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>TradeCycle - Homepage</title>
+    <title>TradeCycle</title>
     <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="responsive.css">
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
-    />    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    />    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   </head>
   <body>
     <header>
       <div class="logo-container">
-        <img src="icons/logo.svg" height="100" width="100" />
+        <img src="icons/logo.svg"/>
         <h1>TradeCycle</h1>
       </div>
       <div class="username-icon-container">
